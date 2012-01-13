eb86c48e86a683013aad8c0d0f50eb264b16af11
Brian McKinney <bmckinney@ellingtoncms.com>
2012-01-12 21:38:06 -0600

    Making sure space is getting striped

--- social_auth/backends/pipeline/user.py
+++ social_auth/backends/pipeline/user.py
@@ -69,7 +69,6 @@ def create_user(backend, details, response, uid, username, user=None, *args,
         #Added by brianmckinney to allow us to only invite manly users
         ###############################################################
         from invite.models import Invitation
-        import pdb; pdb.set_trace()
         invite_key = kwargs['request'].session.get('invite', "").strip()
  
         if invite_key:
